37b7fd18f4e89ecfd08e393c41cc4a78ae61f374
Giulio Franco Sartor <giulio.f.sartor.dev@gmail.com>
2022-11-07 17:25:08 +0100

    commit command with add, push and merge options

---
+++ commands/commit.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+ggit_commands_dir="$(dirname "$0")"
+
+help_message="Usage: ggit commit [<options>] [<branch>...]
+
+Commit changes into current branch
+
+Options:
+  -a, --add         Add changes to staging
+  -m, --message     Commit with message
+  -mt, --mergeto    Merge to other branches after commit
+  -p, --push        Push commit to remote
+  -h, --help        Print usage help message
+"
+
+working_branch=$(git rev-parse --abbrev-ref HEAD)
+
+message=""
+add=0
+merge=0
+merge_branches=()
+push=0
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-a|--add)
+			add=1
+			shift
+			;;
+		-m|--message)
+			message="$2"
+			shift
+			shift
+			;;
+		-mt|--mergeto)
+			merge=1
+			shift
+			while [[ $# -gt 0 ]] ; do
+				if [[ "$1" == -* ]] ; then
+					break
+				fi
+				merge_branches+=("$1")
+				shift
+			done
+			;;
+		-p|--push)
+			push=1
+			shift
+			;;
+		-h|--help)
+			echo "$help_message"
+			exit 0
+			;;
+		-*|--**)
+			echo "invalid option $1"
+			exit 1
+			;;
+		*)
+			echo "invalid argument $1"
+			exit 1
+			;;
+	esac
+done
+
+if (( "$add" == 1 )) ; then
+	git add .
+fi
+
+if [ "$message" != "" ] ; then
+	git commit -m "$message"
+else
+	git commit
+fi
+
+if (( "$push" == 1 )) ; then
+	git push
+fi
+
+if (( "$merge" == 1 )) ; then
+	if (( "$push" == 1 )) ; then
+		"$ggit_commands_dir"/mergeto.sh ${merge_branches[@]} --push
+	else
+		"$ggit_commands_dir"/mergeto.sh ${merge_branches[@]}
+	fi
+fi
--- ggit.sh
+++ ggit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 ggit_dir="$(dirname "$0")"
+ggit_commands_dir="$ggit_dir/commands"
 
 help_message="Usage: ggit <command> [<args>] 
 
 Commands:
+	commit         Commit changes
 	mergeto, mt    Merge current branch into one or more branches
   pull           Multibranch pull
 
@@ -14,14 +16,19 @@ Options:
 
 while [[ $# -gt 0 ]]; do
 	case $1 in
+		commit)
+			shift
+			"$ggit_commands_dir"/commit.sh "$@"
+			exit $?
+			;;
 		mt|mergeto)
 			shift
-			"$ggit_dir"/commands/mergeto.sh $@
+			"$ggit_commands_dir"/mergeto.sh "$@"
 			exit $?
 			;;
 		pull)
 			shift
-			"$ggit_dir"/commands/pull.sh $@
+			"$ggit_commands_dir"/pull.sh "$@"
 			exit $?
 			;;
 		-h|--help)
